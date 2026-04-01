83a9068fc5a3616a975938e51aea6eab2d70a29b
1n1tial <dan.yun0821@gmail.com>
2026-04-01 17:21:04 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -7,17 +7,19 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+   if (cnt == M) {
+      for (int i=0; i<M; i++) cout << vec[i] << ' ';
+      cout << '\n';
+      return;
+   }
+   for (int i = (cnt ? vec.back()+1 : 1); i<=N; i++) {
+      vec.push_back(i);
+      func(cnt+1);
+      vec.pop_back();
+   }
 }
 
 int main() {
    cin >> N >> M;
    func(0);
-   return 0;
 }
